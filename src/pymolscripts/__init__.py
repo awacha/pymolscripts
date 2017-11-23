b257e218f756691fafb8d3a354d51a0f2257e457
András Wacha <awacha@gmail.com>
2017-11-23 17:14:57 +0100

    Can draw an axes, idea stolen from pymolwiki

--- src/pymolscripts/__init__.py
+++ src/pymolscripts/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, unicode_literals, print_function, with_statement
 from . import pymol_fig_manager
-from . import bilayertools, pymol_babel, recognize_peptide, savegro, setbetahelix, structure_cleaning, hbonds, utils
+from . import bilayertools, pymol_babel, recognize_peptide, savegro, setbetahelix, structure_cleaning, hbonds, utils, axes
