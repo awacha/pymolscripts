fc286c162824c04597fe62d001d1a5c40f66af4f
András Wacha <awacha@gmail.com>
2017-11-23 17:13:50 +0100

    Utilities with hydrogen bonds: creating restraints, constraints and listing.

--- src/pymolscripts/hbonds.py
+++ src/pymolscripts/hbonds.py
@@ -1,4 +1,5 @@
 from pymol import cmd
+from .utils import iterate_indices
 
 def iterate_hbonds(selection, name='__hbonds'):
     # idea stolen from http://pymolwiki.org/index.php/get_raw_distances, Takanori Nakane and Thomas Holder
@@ -19,6 +20,44 @@ def generate_hbond_constraints(selection, filename, name='__hbonds'):
     with open(filename, 'wt') as f:
         f.write('[ constraints ]\n')
         for idx1, idx2, dist in iterate_hbonds(selection, name):
-            f.write('{:10d}{:10d}{:10.4f}\n'.format(idx1[1],idx2[1],dist))
+            f.write('{:10d}{:10d} 2 {:10.4f}\n'.format(idx1[1],idx2[1],dist/10.))
+
+def generate_hbond_restraints(selection, filename, strength=1000, name='__hbonds'):
+    strength=float(strength)
+    with open(filename, 'wt') as f:
+        f.write('[ bonds ]\n')
+        for idx1, idx2, dist in iterate_hbonds(selection, name):
+            f.write('{:10d}{:10d} 6 {:10.4f} {:.6f}\n'.format(idx1[1],idx2[1],dist/10.,strength))
+
+
+def find_hbonds(selection_hydrogen, selection_acceptor, dmin=1, dmax=3):
+    dmin = float(dmin)
+    dmax=float(dmax)
+    for oxygen in iterate_indices(selection_acceptor):
+        hydrogendists = [(h, cmd.get_distance('idx {}'.format(oxygen),
+                                              'idx {}'.format(h))) for h in iterate_indices(selection_hydrogen)]
+        hydrogendists = [(h,d) for h,d in hydrogendists if d>=dmin and d<=dmax]
+        if not hydrogendists:
+            continue
+        hydrogen, dist = sorted(hydrogendists, key=lambda x:x[1])[0]
+        # this hydrogen is the nearest one to this oxygen. Try it the other way round
+        oxygendists = [(o, cmd.get_distance('idx {}'.format(hydrogen),
+                                            'idx {}'.format(o))) for o in iterate_indices(selection_acceptor)]
+        oxygendists = [(o, d) for o,d in oxygendists if d >=dmin and d<=dmax]
+        assert oxygendists
+        o1, dist1 = sorted(oxygendists, key = lambda x:x[1])[0]
+        if o1==oxygen:
+            yield (oxygen, hydrogen, dist)
+
+def beta_hbonds(selection_hydrogen, selection_acceptor, dmin=1, dmax=3):
+    i=0
+    for o, h, d in find_hbonds(selection_hydrogen, selection_acceptor, dmin, dmax):
+        cmd.distance('dist{:04d}'.format(i), 'idx {}'.format(o), 'idx {}'.format(h), mode=0)
+        cmd.group('hbonds', 'dist{:04d}'.format(i))
+        i+=1
+
+cmd.extend('beta_hbonds', beta_hbonds)
 
 cmd.extend('generate_hbond_constraints', generate_hbond_constraints)
+
+cmd.extend('generate_hbond_restraints', generate_hbond_restraints)
