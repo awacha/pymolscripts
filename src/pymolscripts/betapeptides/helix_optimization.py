2de11f1afa496a3e447ccb093e81d6073943380a
András Wacha <awacha@gmail.com>
2018-05-16 13:49:07 +0200

    Some tuning in helix optimization procedure

--- src/pymolscripts/betapeptides/helix_optimization.py
+++ src/pymolscripts/betapeptides/helix_optimization.py
@@ -3,15 +3,19 @@ import scipy.optimize
 from pymol import cmd
 from .setbetahelix import helicize_beta_peptide
 from ..utils import get_atom_parameters
+from typing import Union, Iterable
+import itertools
 
 
 def get_hbdists(angles, resmin, resmax, resistep, selection='all', helicize=True):
     if helicize:
-        helicize_beta_peptide('({}, {}, {})'.format(*angles), selection)
+        helicize_beta_peptide(angles, selection)
     dists = []
-    for r in range(resmin, resmax + 1):
+    for r, rstep in zip(range(resmin, resmax + 1), itertools.cycle(resistep)):
+        if rstep == 0: continue
+        if r<resmin or r+rstep<resmin or r>resmax or r+rstep>resmax: continue
         selections = ['({}) and resi {} and name HN'.format(selection, r),
-                      '({}) and resi {} and name O'.format(selection, r + resistep)]
+                      '({}) and resi {} and name O'.format(selection, r + rstep)]
         if not all([cmd.count_atoms(sel) for sel in selections]):
             continue
         dists.append(cmd.get_distance(*selections))
@@ -19,11 +23,13 @@ def get_hbdists(angles, resmin, resmax, resistep, selection='all', helicize=True
 
 def get_dadists(angles, resmin, resmax, resistep, selection='all', helicize=True):
     if helicize:
-        helicize_beta_peptide('({}, {}, {})'.format(*angles), selection)
+        helicize_beta_peptide(angles, selection)
     dists = []
-    for r in range(resmin, resmax + 1):
+    for r, rstep in zip(range(resmin, resmax + 1), itertools.cycle(resistep)):
+        if rstep == 0: continue
+        if r<resmin or r+rstep<resmin or r>resmax or r+rstep>resmax: continue
         selections = ['({}) and resi {} and name N'.format(selection, r), # donor
-                      '({}) and resi {} and name O'.format(selection, r + resistep)] # acceptor
+                      '({}) and resi {} and name O'.format(selection, r + rstep)] # acceptor
         if not all([cmd.count_atoms(sel) for sel in selections]):
             continue
         dists.append(cmd.get_distance(*selections))
@@ -32,13 +38,15 @@ def get_dadists(angles, resmin, resmax, resistep, selection='all', helicize=True
 
 def get_non_collinearity(angles, resmin, resmax, resistep, selection='all', helicize=True):
     if helicize:
-        helicize_beta_peptide('({}, {}, {})'.format(*angles), selection)
+        helicize_beta_peptide(angles, selection)
     angles = []
-    for r in range(resmin, resmax + 1):
+    for r, rstep in zip(range(resmin, resmax + 1), itertools.cycle(resistep)):
+        if rstep==0: continue
+        if r<resmin or r+rstep<resmin or r>resmax or r+rstep>resmax: continue
         selections = ['({}) and resi {} and name N'.format(selection, r),
                       '({}) and resi {} and name HN'.format(selection, r),
-                      '({}) and resi {} and name O'.format(selection, r + resistep),
-                      '({}) and resi {} and name C'.format(selection, r + resistep)]
+                      '({}) and resi {} and name O'.format(selection, r + rstep),
+                      '({}) and resi {} and name C'.format(selection, r + rstep)]
         if any([cmd.count_atoms(sel) == 0 for sel in selections]):
             continue
         coords = [cmd.get_coords(sel) for sel in selections]
@@ -51,12 +59,14 @@ def get_non_collinearity(angles, resmin, resmax, resistep, selection='all', heli
 
 def get_hbangles(angles, resmin, resmax, resistep, selection='all', helicize=True):
     if helicize:
-        helicize_beta_peptide('({}, {}, {})'.format(*angles), selection)
+        helicize_beta_peptide(angles, selection)
     angles = []
-    for r in range(resmin, resmax + 1):
+    for r, rstep in zip(range(resmin, resmax + 1), itertools.cycle(resistep)):
+        if rstep == 0: continue
+        if r<resmin or r+rstep<resmin or r>resmax or r+rstep>resmax: continue
         selections = ['({}) and resi {} and name N'.format(selection, r),
                       '({}) and resi {} and name HN'.format(selection, r),
-                      '({}) and resi {} and name O'.format(selection, r + resistep)]
+                      '({}) and resi {} and name O'.format(selection, r + rstep)]
         if any([cmd.count_atoms(sel) == 0 for sel in selections]):
             continue
         angles.append(cmd.get_angle(*selections))
@@ -64,9 +74,9 @@ def get_hbangles(angles, resmin, resmax, resistep, selection='all', helicize=Tru
 
 
 def targetfunc(angles, resmin, resmax, resistep, hbondlength, selection):
-    #    print('Targetfunc: {}, {}, {}'.format(angles, resmin, resmax))
-    dists = get_hbdists(angles, resmin, resmax, resistep, selection, helicize=True)
-    hbangles = get_hbangles(angles, resmin, resmax, resistep, selection, helicize=False)
+    angles_ = [(angles[3*i+0], angles[3*i+1], angles[3*i+2]) for i in range(len(angles)//3)]
+    dists = get_hbdists(angles_, resmin, resmax, resistep, selection, helicize=True)
+    hbangles = get_hbangles(angles_, resmin, resmax, resistep, selection, helicize=False)
     if any([a<120 for a in hbangles]):
         return 1000
     val = np.sqrt(np.sum(dists - hbondlength) ** 2) ** 0.5
@@ -82,7 +92,8 @@ def targetfunc_hbondlen(hbondlen, init, resmin, resmax, resistep, selection):
     return targetfunc(result.x, resmin, resmax, resistep, hbondlen[0], selection)
 
 
-def optimize_beta_helix(selection, initphi:float, inittheta:float, initpsi:float, resistep:int, hbondlen:float):
+def optimize_beta_helix(selection, initphi:float, inittheta:float, initpsi:float,
+                        resistep:Union[int,Iterable[int]], hbondlen:float):
     """
     DESCRIPTION
 
@@ -105,7 +116,8 @@ def optimize_beta_helix(selection, initphi:float, inittheta:float, initpsi:float
 
         resistep: the number of residues a hydrogen bond crosses. I.e. the hydrogen of the
             i-th residue is bonded to the oxygen of the i+<resistep>-th residue. Can be
-            negative of course.
+            negative of course. Alternatively, it can be a list of integers, which will be
+            applied consecutively for the amino acid components, cycling if exhausted.
 
         hbondlen: the desired hydrogen bond length.
     """
@@ -113,51 +125,28 @@ def optimize_beta_helix(selection, initphi:float, inittheta:float, initpsi:float
         print('   {}: {}'.format(label, ', '.join(['{:.2f}'.format(v) for v in values])))
         print('       {:.2f} \xb1 {:.2f} ({:.2f} to {:.2f})'.format(np.mean(values), np.std(values), np.min(values),
                                                                     np.max(values)))
-    init = [float(initphi), float(inittheta), float(initpsi)]
     atompars = get_atom_parameters(selection)
     resimin = min([a['resv'] for a in atompars])
     resimax = max([a['resv'] for a in atompars])
-    resistep = int(resistep)
+    if isinstance(resistep, str):
+        resistep=resistep.strip()
+        if (resistep.startswith('(') and resistep.endswith(')')) or (resistep.startswith('[') and resistep.endswith(']')):
+            resistep = [int(x.strip()) for x in resistep[1:-1].split(',')]
+        else:
+            resistep = [int(resistep)]
+    elif isinstance(resistep, int):
+        resistep = [resistep]
+    init = [float(initphi), float(inittheta), float(initpsi)]*len(resistep)
     hbondlen = float(hbondlen)
     result = scipy.optimize.minimize(targetfunc, np.array(init), args=(resimin, resimax, resistep, hbondlen, selection),
                                      method='Nelder-Mead')
     badness = targetfunc(result.x, resimin, resimax, resistep, hbondlen, selection)
-    hbangles = get_hbangles(result.x, resimin, resimax, resistep, selection, helicize=False)
-    dists = get_hbdists(result.x, resimin, resimax, resistep, selection, helicize=False)
-    print('L_hb={:.3f} A: badness: {:.3f} Angles: {:.2f}, {:.2f}, {:.2f}'.format(hbondlen, badness, *result.x))
+    angles_ = [(result.x[3*i+0], result.x[3*i+1], result.x[3*i+2]) for i in range(len(result.x)//3)]
+    hbangles = get_hbangles(angles_, resimin, resimax, resistep, selection, helicize=False)
+    dists = get_hbdists(angles_, resimin, resimax, resistep, selection, helicize=False)
+    print('L_hb={:.3f} A: badness: {:.3f} Angles: {}'.format(
+        hbondlen, badness,
+        ', '.join(['({:.2f}, {:.2f}, {:.2f})'.format(*a) for a in angles_])))
     print_summary(dists, 'Distances')
     print_summary(hbangles, 'Hydrogen bond angles')
-    return list(result.x)
-
-if __name__ == '__main__':
-    init = [-140.3, 66.5, -136.8]
-    resistep = 2
-    # init = [-134.457, 50.916, -136.7757]
-    # targetfunc(init, 0,9)
-    # result = scipy.optimize.minimize(targetfunc, np.array(init), args=(0, 9, 1.5), method='Nelder-Mead')
-    # print(result.x)
-
-    results = []
-    for hblen in np.linspace(1.5, 3, 15):
-        result = scipy.optimize.minimize(targetfunc, init, args=(0, 9, resistep, hblen), method='Nelder-Mead')
-        dists = get_hbdists(result.x, 0, 9, resistep)
-        badness = targetfunc(result.x, 0, 9, resistep, hblen)
-        coll_angles = get_non_collinearity(result.x, 0, 9, resistep)
-        hbangles = get_hbangles(result.x, 0, 9, resistep)
-        results.append((hblen, result, dists, badness, coll_angles, hbangles))
-
-
-
-
-    for hblen, result, dists, badness, coll_angles, hbangles in results:
-        print('L_hb={:.3f} A: badness: {:.3f} Angles: {:.2f}, {:.2f}, {:.2f}'.format(hblen, badness, *result.x))
-        print_summary(dists, 'Distances')
-        #    print_summary(coll_angles, 'Collinearity')
-        print_summary(hbangles, 'Hydrogen bond angles')
-
-    # result= scipy.optimize.minimize(targetfunc_hbondlen, [2.0], args=(init, 0, 9, resistep))
-    # hbondlen = result.x[0]
-    # print('Optimum hydrogen bond length:', hbondlen)
-
-    # result = scipy.optimize.minimize(targetfunc, init, args=(0, 9, resistep, hbondlen))
-    # print('Optimum torsion angles: ',result.x)
+    return angles_
